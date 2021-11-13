195a5e19c6f15868b8ca09d4e92970c421592be4
Xy Ren <xy.r@outlook.com>
2021-11-13 17:57:45 +0800

    Demonstrate that scoped effects are broken

---
+++ example/Broker.hs
@@ -0,0 +1,31 @@
+module Broker where
+
+import           Cleff
+import           Control.Concurrent (forkIO, threadDelay)
+import           Control.Monad      (void)
+
+nestedImpl :: Int -> (String -> (Bool -> IO ()) -> IO ()) -> IO ()
+nestedImpl i cb = void . forkIO $ do
+  threadDelay $ 1 * 1000 * 1000
+  cb (show i) \b -> putStrLn $ "result " ++ show b
+
+data Broker :: Effect where
+  Subscribe :: Int -> (String -> m Bool) -> Broker m ()
+
+subscribe :: Broker :> es => Int -> (String -> Eff es Bool) -> Eff es ()
+subscribe channel = send . Subscribe channel
+
+runBroker :: IOE :> es => Eff (Broker : es) a -> Eff es a
+runBroker = interpretIO \case
+  Subscribe channel cb -> liftIO do
+    putStrLn $ "Subscribe: " ++ show channel
+    nestedImpl channel \s icb -> icb =<< runInIO (cb s)
+
+prog :: (IOE :> es, Broker :> es) => Eff es ()
+prog = do
+  subscribe 1 \_ -> pure True
+  subscribe 2 \_ -> pure False
+  liftIO $ threadDelay $ 3 * 1000 * 1000
+
+runProg :: IO ()
+runProg = runIOE $ runBroker prog
--- example/Main.hs
+++ example/Main.hs
@@ -1,4 +1,5 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "This is cleff's example module! Browse /example to get started with the library."
+main = do
+  putStrLn "This is cleff's example module! Browse /example to get started with the library."
